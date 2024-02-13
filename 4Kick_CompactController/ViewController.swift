b168c0fdc8f11e9bba25fba57a126692647ac349
m.nechaev <m.nechaev@ritm.media>
2024-02-13 16:32:34 +0300

    Initial Commit

---
+++ 4Kick_CompactController/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  4Kick_CompactController
+//
+//  Created by Нечаев Михаил on 13.02.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
